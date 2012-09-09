8a6be01c1a575f027f1b8be953a5d8f996ee3b89
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-09-09 17:43:32 +0100

    Initial commit with blank Foundation template and Knockback + dependencies

---
+++ app/trask/js/app.js
@@ -0,0 +1,69 @@
+/* Foundation */
+(function($){
+  $(function(){
+    $(document).foundationMediaQueryViewer();
+    $(document).foundationAlerts();
+    $(document).foundationAccordion();
+    $(document).tooltips();
+    $('input, textarea').placeholder();
+    $(document).foundationButtons();
+    $(document).foundationNavigation();
+    $(document).foundationCustomForms();
+    $(document).foundationTabs({callback:$.foundation.customForms.appendCustomMarkup});
+  });
+
+})(jQuery);
+
+/* Trask */
+(function() {
+
+  $(function() {
+    ko.bindingHandlers.dblclick = {
+      init: function(element, value_accessor) {
+        return $(element).dblclick(ko.utils.unwrapObservable(value_accessor()));
+      }
+    };
+
+    ko.bindingHandlers.block = {
+      update: function(element, value_accessor) {
+        return element.style.display = ko.utils.unwrapObservable(value_accessor()) ? 'block' : 'none';
+      }
+    };
+
+    ko.bindingHandlers.selectAndFocus = {
+      init: function(element, value_accessor, all_bindings_accessor) {
+        ko.bindingHandlers.hasfocus.init(element, value_accessor, all_bindings_accessor);
+        return ko.utils.registerEventHandler(element, 'focus', function() {
+          return element.select();
+        });
+      },
+      update: function(element, value_accessor) {
+        var _this = this;
+        ko.utils.unwrapObservable(value_accessor());
+        return _.defer(function() {
+          return ko.bindingHandlers.hasfocus.update(element, value_accessor);
+        });
+      }
+    };
+
+    window.app = {
+      viewmodels: {},
+      collections: {}
+    };
+
+    app.viewmodels.settings = new SettingsViewModel();
+    app.collections.todos = new TodoCollection();
+    app.viewmodels.header = new HeaderViewModel(app.collections.todos);
+    app.viewmodels.todos = new TodosViewModel(app.collections.todos);
+    app.viewmodels.footer = new FooterViewModel(app.collections.todos);
+
+    ko.applyBindings(app.viewmodels, $('#todoapp')[0]);
+
+    new AppRouter();
+
+    Backbone.history.start();
+
+    return app.collections.todos.fetch();
+  });
+
+}).call(this);
